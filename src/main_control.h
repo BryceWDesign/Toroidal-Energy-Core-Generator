b73e6b893dddbaca6b9e58eabba2184cf7c6f172
Bryce Wooster <40742705+BryceWDesign@users.noreply.github.com>
2025-05-27 13:57:07 -0700

    Add header for main control loop functions

---
+++ src/main_control.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_CONTROL_H
+#define MAIN_CONTROL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void setup(void);
+void loop(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MAIN_CONTROL_H
